59234704584d3a021331d0f0ea726b52169003c5
jnj <jiang.2826379@163.com>
2024-09-30 16:28:47 +0800

    update

---
+++ test-k8s-executor.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+import logging
+import os
+
+import pendulum
+
+from airflow.configuration import conf
+from airflow.decorators import task
+from airflow.example_dags.libs.helper import print_stuff
+from airflow.models.dag import DAG
+
+log = logging.getLogger(__name__)
+
+
+try:
+    from kubernetes.client import models as k8s
+except ImportError:
+    log.warning(
+        "The example_kubernetes_executor example DAG requires the kubernetes provider."
+        " Please install it with: pip install apache-airflow[cncf.kubernetes]"
+    )
+    k8s = None
+
+
+
+if k8s:
+    with DAG(
+        dag_id="my_example_kubernetes_executor",
+        schedule=None,
+        start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+        catchup=False,
+        tags=["k8s-executor-example"],
+    ) as dag:
+        # You can use annotations on your kubernetes pods!
+        start_task_executor_config = {
+            "pod_override": k8s.V1Pod(metadata=k8s.V1ObjectMeta(annotations={"test": "annotation"}))
+        }
+
+
+        @task(executor_config=start_task_executor_config)
+        def start_task():
+            print_stuff()
+    
+    start_task=()
