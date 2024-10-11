8a66570c9a54092fea53236ced111d5e30c70c18
jnj <jiang.2826379@163.com>
2024-10-11 12:26:22 +0800

    update

--- k8s-spark-demo.py
+++ k8s-spark-demo.py
@@ -14,6 +14,7 @@ k8s_exec_config_resource_requirements = {
             containers=[
                 k8s.V1Container(
                     name="base",
+                    image="swr.ap-southeast-3.myhuaweicloud.com/dmetasoul-repo/jupyter:v1.0.4",
                     resources=k8s.V1ResourceRequirements(
                         requests={"cpu": 0.5, "memory": "200Mi", "ephemeral-storage": "1Gi"},
                         limits={"cpu": 0.5, "memory": "200Mi", "ephemeral-storage": "1Gi"}
@@ -25,15 +26,24 @@ k8s_exec_config_resource_requirements = {
 }
 
 with DAG(
-    dag_id="spark-k8s",
+    dag_id="k8s-spark-demo",
     schedule=None,
     start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
     catchup=False
 ):
 
     @task(executor_config=k8s_exec_config_resource_requirements)
-    def resource_requirements_override_example():
-        print_stuff()
+    def spark_example():
         print("Hello, World!")
+        # from pyspark.sql import SparkSession
+        # spark = SparkSession \
+        #         .builder \
+        #         .master('local[1]') \
+        #         .appName("data loading for feast") \
+        #         .config("spark.executor.instances", "1") \
+        #         .config("spark.executor.memory", "1g") \
+        #         .config("spark.sql.legacy.parquet.nanosAsLong", "true") \
+        #         .getOrCreate()
+        # spark.sql('show tables').show()
 
-    resource_requirements_override_example()
+    spark_example()
