ee4d5cb7d382623f3e882913ea4514adc8a01071
jnj <jiang.2826379@163.com>
2024-10-12 16:42:23 +0800

    update

---
+++ k8s-spark-sql-read-demo.py
@@ -0,0 +1,68 @@
+import pendulum
+import time
+
+from airflow.models.dag import DAG
+from airflow.decorators import task
+from airflow.operators.bash import BashOperator
+from airflow.example_dags.libs.helper import print_stuff
+from kubernetes.client import models as k8s
+
+
+k8s_exec_config_resource_requirements = {
+    "pod_override": k8s.V1Pod(
+        spec=k8s.V1PodSpec(
+            containers=[
+                k8s.V1Container(
+                    name="base",
+                    image="swr.ap-southeast-3.myhuaweicloud.com/dmetasoul-repo/jupyter:v1.0.5-airflow-v3",
+                    command=["/bin/bash", "/opt/run-airflow.sh"],
+                    volume_mounts=[
+                        k8s.V1VolumeMount(name="ephemeral-volume", mount_path="/home/jovyan")
+                    ],
+                    resources=k8s.V1ResourceRequirements(
+                        requests={"cpu": 0.5, "memory": "200Mi", "ephemeral-storage": "1Gi"},
+                        limits={"cpu": 0.5, "memory": "200Mi", "ephemeral-storage": "1Gi"},
+                    ),
+                    
+                )
+            ],
+            volumes=[
+                k8s.V1Volume(
+                    name="ephemeral-volume",
+                    ephemeral=k8s.V1EphemeralVolumeSource(
+                        volume_claim_template=k8s.V1PersistentVolumeClaimTemplate(
+                            spec=k8s.V1PersistentVolumeClaimSpec(
+                                access_modes=["ReadWriteOnce"],
+                                storage_class_name="csi-disk",
+                                resources=k8s.V1ResourceRequirements(requests={"storage": "10Gi"}),
+                            )
+                        )
+                    ),
+                )
+            ],
+        )
+    )
+}
+
+
+with DAG(
+    dag_id="k8s-spark-sql-demo",
+    schedule=None,
+    start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
+    catchup=False,
+):
+
+    @task(executor_config=k8s_exec_config_resource_requirements)
+    def sparksql_example():
+        from pyspark.sql import SparkSession
+        spark = SparkSession \
+                .builder \
+                .master('local[1]') \
+                .appName("data loading for feast") \
+                .config("spark.executor.instances", "1") \
+                .config("spark.executor.memory", "1g") \
+                .getOrCreate()
+        df = spark.read.csv('/opt/winequality-red.csv', header=True, inferSchema=True, sep=';')
+        df.printSchema()
+        df.show()
+    sparksql_example()
