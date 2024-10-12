09a09ce9cc33cf0542da352ffd5811309d766389
jnj <jiang.2826379@163.com>
2024-10-12 12:07:04 +0800

    update

--- k8s-spark-demo.py
+++ k8s-spark-demo.py
@@ -14,8 +14,8 @@ k8s_exec_config_resource_requirements = {
             containers=[
                 k8s.V1Container(
                     name="base",
-                    image="swr.ap-southeast-3.myhuaweicloud.com/dmetasoul-repo/jupyter:v1.0.5-airflow-v1",
-                    command=["/bin/bash", "/home/jovyan/run-airflow.sh"],
+                    image="swr.ap-southeast-3.myhuaweicloud.com/dmetasoul-repo/jupyter:v1.0.5-airflow-v2",
+                    command=["/bin/bash", "/opt/run-airflow.sh"],
                     volume_mounts=[
                         k8s.V1VolumeMount(name="ephemeral-volume", mount_path="/home/jovyan")
                     ],
